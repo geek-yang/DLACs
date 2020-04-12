0b5c2331417436f0aa18b2c4ddf47afb811bc95b
ESLT0068 <ESLT0068@localhost.localdomain>
2020-04-12 16:39:14 +0200

    example of forecast Lorenz 84 model with BayesConvLSTM

--- dlacs/saveNetCDF.py
+++ dlacs/saveNetCDF.py
@@ -4,7 +4,7 @@ Copyright Netherlands eScience Center
 Function        : Save Output Files into NetCDF files
 Author          : Yang Liu (y.liu@esciencecenter.nl)
 First Built     : 2020.03.11
-Last Update     : 2020.03.11
+Last Update     : 2020.04.10
 Contributor     :
 Description     : This module aims to save the output fields into the netCDF files.
                   It outputs netCDF 4 files. The data has been compressed.
@@ -65,6 +65,43 @@ class savenc:
         lat_wrap_var[:] = lat
         lon_wrap_var[:] = lon
         pred_wrap_var[:] = numpyMatrix
+        # close the file
+        data_wrap.close()
+        print("Create netcdf files successfully!!")
+
+    def ncfile_Lorenz(self, numpyMatrix):
+        """
+        Create nc4 file with given dataset. The dataset should contain 4 dimensions.
+        This module is specifically designed for Lorenz 84 model output.
+        """
+        data_wrap = Dataset(os.path.join(self.datapath,self.fileName), 'w',format = 'NETCDF4')
+        # get the dimension of input data
+        var, time, lead = numpyMatrix.shape
+        # create dimensions for netcdf data
+        var_wrap_dim = data_wrap.createDimension('var',var)
+        time_wrap_dim = data_wrap.createDimension('time',time)
+        lead_wrap_dim = data_wrap.createDimension('lead',lead)
+        # create 1-dimension variables
+        var_wrap_var = data_wrap.createVariable('var',np.int32,('var',))
+        time_wrap_var = data_wrap.createVariable('time',np.int32,('time',))
+        lead_wrap_var = data_wrap.createVariable('lead',np.int32,('lead',))
+        # create 3-dimension variables
+        series_wrap_var = data_wrap.createVariable('series',np.float32,('var', 'time', 'lead'))
+        # global attributes
+        data_wrap.description = 'Lead time dependent Lorenz 84 forecast'
+        # variable attributes
+        var_wrap_var.units = '1'
+        time_wrap_var.units = '1'
+        lead_wrap_var.units = '1'
+
+        var_wrap_var.long_name = 'number of variables'
+        time_wrap_var.long_name = 'time span'
+        lead_wrap_var.long_name = 'lead time'
+        # writing data
+        var_wrap_var[:] = var
+        time_wrap_var[:] = time
+        lead_wrap_var[:] = lead
+        series_wrap_var[:] = numpyMatrix
         # close the file
         data_wrap.close()
         print("Create netcdf files successfully!!")
